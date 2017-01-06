da46a9dccb6a32d7d24f1ff7b2b632c39ec92c0e
Simon Zachau <zachau@kth.se>
2017-01-06 17:27:26 +0100

    window of new tab must be active in order to fire handleUrl

--- chrome.js
+++ chrome.js
@@ -1,12 +1,22 @@
+// load new tab
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	// is updated tab active?
-	if (changeInfo.url != null && tab.active == true) {
-		timeTrack.handleUrl(changeInfo.url);
-	}
+	chrome.windows.get(tab.windowId, null, function(window) {
+		// is updated tab active and is its window active?
+		if(tab.active && window.focused && changeInfo.url != null) {
+			timeTrack.handleUrl(changeInfo.url);
+		}
+	});
 });
+
+// switch tab
 
 chrome.tabs.onActivated.addListener(function (activeInfo) {
 	chrome.tabs.get(activeInfo.tabId, function (tab) {
 		timeTrack.handleUrl(tab.url);
 	});
 });
+
+// switch window
+
+// TODO
