391a7a0f5d1a6f8090cf3e3c0498b2f41e7dbba7
Simon Zachau <zachau@in.tum.de>
2017-05-02 18:46:23 +0200

    updated dependencies in order to match the unminified versions of the review process of addons.mozilla.org

--- chrome/popup_chrome.js
+++ chrome/popup_chrome.js
@@ -2,27 +2,25 @@ function getBackground() {
 	return chrome.extension.getBackgroundPage();
 }
 
-const storage = chrome.storage.local;
+// function setPreference(key, value) {
+// 	return new Promise(function(resolve, reject) {
+// 		let preference = {};
+// 		preference[key] = value;
+// 		chrome.storage.local.set(preference, function() {
+// 			if(chrome.runtime.lastError) {
+// 				reject(chrome.runtime.lastError.message);
+// 			} else {
+// 				resolve();
+// 			}
+// 		});
+// 	});
+// }
 
-function setPreference(key, value) {
-	return new Promise(function(resolve, reject) {
-		let preference = {};
-		preference[key] = value;
-		storage.set(preference, function() {
-			if(chrome.runtime.lastError) {
-				reject(chrome.runtime.lastError.message);
-			} else {
-				resolve();
-			}
-		});
-	});
-}
-
-function getPreference(key, callback) {
-	return new Promise(function(resolve, reject) {
-		storage.get(key, function(preference) {
-			const value = preference[key];
-			resolve(value);
-		});
-	});
-}
+// function getPreference(key, callback) {
+// 	return new Promise(function(resolve, reject) {
+// 		chrome.storage.local.get(key, function(preference) {
+// 			const value = preference[key];
+// 			resolve(value);
+// 		});
+// 	});
+// }
